632c1edd718d6e064f0c7815ad4476ce000bb220
Николай <usenkko.nikolay@gmail.com>
2025-10-11 14:21:18 +0300

    Добавьте файлы проекта.

---
+++ Software-Lab1-Task1/Par.cs
@@ -0,0 +1,24 @@
+﻿class Par
+{
+    private double a, b, c;
+    public double GetV() => a * b * c;
+    public double GetS() => 2 * (a * b + b * c + a * c);
+    public void Info()
+    {
+        String str =
+            "\n*******************************\n" +
+            "*                             *\n" +
+            "*       Параллелепипед        *\n" +
+            "*                             *\n" +
+            "*******************************\n";
+        Console.WriteLine(str);
+        Console.WriteLine(string.Format("Значения сторон параллелепипеда:\nA = {0:0.00};\nB = {1:0.00};\nC = {2:0.00}.", a, b, c));
+        Console.WriteLine(string.Format("Объём и площадь поверхности параллелепипеда:\nV = {0:0.00}\nS = {1:0.00}", GetV(), GetS()));
+    }
+    public void Load()
+    {
+        a = Convert.ToDouble(Console.ReadLine());
+        b = Convert.ToDouble(Console.ReadLine());
+        c = Convert.ToDouble(Console.ReadLine());
+    }
+}
---
+++ Software-Lab1-Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+#if !DEBUG
+        TextWriter save_out = Console.Out;
+        TextReader save_in = Console.In;
+        var new_out = new StreamWriter(@"par_output.txt");
+        var new_in = new StreamReader(@"par_input.txt");
+        Console.SetOut(new_out);
+        Console.SetIn(new_in);
+#endif
+        Par p = new Par();
+        p.Load();
+        p.Info();
+#if !DEBUG
+        Console.SetOut(save_out); new_out.Close();
+        Console.SetIn(save_in); new_in.Close();
+#else
+        Console.ReadKey();
+#endif
+    }
+}
